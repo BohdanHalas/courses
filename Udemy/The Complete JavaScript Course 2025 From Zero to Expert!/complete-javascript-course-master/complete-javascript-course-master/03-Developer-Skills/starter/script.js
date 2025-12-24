caf79c29dbddd3a9427dca8d5212ed9042a73b8c
Bohdan.Halas <bohdan.halas10@gmail.com>
2025-12-24 20:08:21 +0100

    nothing new

--- Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -1,3 +1,8 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = '23';
+if (x === 23) console.log(x);
+
+const calcAge = birthYear => 2037 - birthYear;
+console.log();
