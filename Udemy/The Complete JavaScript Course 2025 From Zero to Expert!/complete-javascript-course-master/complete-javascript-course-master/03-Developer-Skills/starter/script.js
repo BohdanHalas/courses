794bfcae9e8e659eee386f72482c1dc2dbf6f7db
Bohdan.Halas <bohdan.halas10@gmail.com>
2026-01-07 13:16:48 +0100

    After lesson 07.01 p1 (Chalenges 1 and 2.0)

--- Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -52,45 +52,83 @@ const temperatures2 = [13, -12, -16, -11, `error`, 19, 11, 11, 11, 114, 19, 51];
 // const amplitudeNew = calcTempAmplitudeNew(temperatures, temperatures2);
 // console.log(amplitudeNew);
 
-const measureKelvin = function () {
-  const measurement = {
-    type: `temp`,
-    unit: `celsicus`,
-    value: 10, // Number(prompt(`Degrees celsius:`)),
+// const measureKelvin = function () {
+//   const measurement = {
+//     type: `temp`,
+//     unit: `celsicus`,
+//     value: 10, // Number(prompt(`Degrees celsius:`)),
+//   };
+
+//   //   console.log(measurement);
+
+//   console.table(measurement);
+//   //   console.log(measurement.value);
+//   //   console.warn(measurement.value);
+//   //   console.error(measurement.value);
+
+//   const kelvin = measurement.value + 273;
+//   return kelvin;
+// };
+
+// console.log(measureKelvin());
+
+// // Using a debugger
+// const calcTempAmplitudeBug = function (t1, t2) {
+//   const temps = t1.concat(t2);
+//   console.log(temps);
+//   let min = temps[0];
+//   let max = temps[0];
+
+//   for (let i = 0; i < temps.length; i++) {
+//     const currentTemp = temps[i];
+
+//     if (typeof currentTemp !== `number`) continue;
+//     //debugger;
+//     if (currentTemp > max) max = currentTemp;
+
+//     if (currentTemp < min) min = currentTemp;
+//   }
+//   console.log(max, min);
+//   return max - min;
+// };
+
+// const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// console.log(amplitudeBug);
+
+//Coding Challenge #1
+
+// Об'єднання стрінгів. Тю
+
+// const printForecast = function (arr) {
+//   let message = `...`;
+//   for (let i = 0; i < arr.length; i++) {
+//     message += ` ${arr[i]}°C in ${i + 1} days ...`;
+//   }
+//   console.log(message);
+// };
+
+// const testArray = [17, 21, 23];
+// const testArray2 = [12, 5, -5, 0, 4];
+
+// printForecast(testArray);
+// printForecast(testArray2);
+
+//Coding Challenge #2
+
+const testArray = [7.5, 8, 6.5, 0, 8.5, 4, 0];
+
+const timeTrackApp = function (arr) {
+  const data = {
+    sum: 0,
+    avg: 0,
+    highDay: ``,
+    numberDays: 0,
+    fullTime: false,
   };
 
-  //   console.log(measurement);
-
-  console.table(measurement);
-  //   console.log(measurement.value);
-  //   console.warn(measurement.value);
-  //   console.error(measurement.value);
-
-  const kelvin = measurement.value + 273;
-  return kelvin;
-};
-
-console.log(measureKelvin());
-
-// Using a debugger
-const calcTempAmplitudeBug = function (t1, t2) {
-  const temps = t1.concat(t2);
-  console.log(temps);
-  let min = temps[0];
-  let max = temps[0];
-
-  for (let i = 0; i < temps.length; i++) {
-    const currentTemp = temps[i];
-
-    if (typeof currentTemp !== `number`) continue;
-    //debugger;
-    if (currentTemp > max) max = currentTemp;
-
-    if (currentTemp < min) min = currentTemp;
+  for (let i = 0; i < arr.length; i++) {
+    data.sum += arr[i];
   }
-  console.log(max, min);
-  return max - min;
+  console.log(data.sum);
 };
-
-const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
-console.log(amplitudeBug);
+timeTrackApp(testArray);
