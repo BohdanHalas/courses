1e8898d827fc19cddeb49c7b7a6062d1773e0a46
Bohdan.Halas <bohdan.halas10@gmail.com>
2026-01-08 20:51:34 +0100

    section 4 (HTML-CSS) - done. I started develop game `guess number` finished 77 lesson udemy

--- Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector(`.message`).textContent = `Correct Number!😎`;
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector(`.number`).textContent = 13;
+document.querySelector(`.score`).textContent = 21;
+
+document.querySelector(`.guess`).value = 23;
+console.log(document.querySelector(`.guess`).value);
+
+*/
+document.querySelector(`.btn.check`).addEventListener(`click`, function () {
+  const guess = Number(document.querySelector(`.guess`).value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = `🙄No Number🙄`;
+  }
+});
