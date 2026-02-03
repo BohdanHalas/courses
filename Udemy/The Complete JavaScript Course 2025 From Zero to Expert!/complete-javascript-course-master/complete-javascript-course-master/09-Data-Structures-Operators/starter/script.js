03c574e7bd9a8265c50f5e3866b9c2f45b3b6e03
Bohdan Halas mac <bohdan.halas10@gmail.com>
2026-02-03 17:14:06 +0100

    after leccion about Spread Operator (...)

--- Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -58,8 +58,59 @@ const restaurant = {
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`,
     );
   },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your declicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+  },
 };
 
+const arr = [7, 8, 9];
+
+const badNewArray = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArray);
+const goodNewArray = [1, 2, ...arr];
+console.log(goodNewArray);
+
+console.log(...goodNewArray);
+console.log(1, 2, 7, 8, 9);
+
+const newMenu = [...restaurant.mainMenu, `Gnocci`];
+console.log(newMenu);
+
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// Join arrays
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+// Iterables: arrays, strings, maps, sets. NOT OBJECTS
+const str = 'Bohdan';
+const letters = [...str, ``, `H.`];
+console.log(letters);
+console.log(...str);
+
+// console.log(`${...str} Halas`); // НЕ МОЖНА
+// Спред (...) використовується лише коли ми створюємо новий масив, або передаємо аргументи в функцію
+
+// Real-world example
+const ingredients = [
+  //   prompt(`Let's make pasta! Ingredient 1?`),
+  //   prompt(`Ingredient 2?`),
+  //   prompt(`Ingredient 3?`),
+];
+
+restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+restaurant.orderPasta(...ingredients);
+
+// ... with Objects
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: `Guiseppe` };
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = `Ristorante Roma`;
+console.log(restaurantCopy);
+console.log(restaurant);
+/*
 restaurant.orderDelivery({
   time: `22:30`,
   address: `Via del Sole, 21`,
@@ -98,7 +149,7 @@ const {
   fri: { open: o, close: c },
 } = openingHours;
 console.log(o, c);
-/*
+
 ////////////////
 const arr = [2, 3, 4];
 const a = arr[0];
