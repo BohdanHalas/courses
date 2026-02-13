56e3d2e18c6833cfeec0b7f86ab6bc288133ea10
Bohdan Halas mac <bohdan.halas10@gmail.com>
2026-02-13 20:50:16 +0100

    after Challenge #3

--- Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -712,3 +712,32 @@ for (const player of game.scored) {
 
 console.log(scorersGemini);
 */
+
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+gameEvents.delete(64);
+console.log(gameEvents);
+
+console.log(
+  `An event happend, on averege, every ${90 / gameEvents.size} minutes`,
+);
+
+for (const [minute, event] of gameEvents) {
+  const half = minute <= 45 ? `FIRST HALF` : `SECOND HALF`;
+  console.log(`[${half}] ${minute}: ${event}`);
+}
