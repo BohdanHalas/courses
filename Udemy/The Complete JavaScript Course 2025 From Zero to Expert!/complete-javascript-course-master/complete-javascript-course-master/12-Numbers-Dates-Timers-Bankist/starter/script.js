32955fa48144e8a98f1715749ec805c324eeff45
Bohdan Halas mac <bohdan.halas10@gmail.com>
2026-03-25 20:34:22 +0100

    after lectures about dilennya z ostacheiy | _ v chuslah | BigInt | Creating Dates

--- Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -293,7 +293,7 @@ console.log(Number.isFinite(23 / 0));
 console.log(Number.isInteger(23));
 console.log(Number.isInteger(23.0));
 console.log(Number.isInteger(23 / 0));
-*/
+
 
 console.log(Math.sqrt(25));
 console.log(25 ** (1 / 2));
@@ -335,3 +335,114 @@ console.log(Math.floor(-23.3));
 console.log((2.7).toFixed(0));
 console.log((2.7).toFixed(3));
 console.log(+(2.345).toFixed(2));
+
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1;
+
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 3 * 2 + 2;
+
+console.log(6 % 2);
+console.log(6 / 2);
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+labelBalance.addEventListener(`click`, function () {
+  [...document.querySelectorAll(`.movements__row`)].forEach(function (row, i) {
+    if (i % 2 === 0) row.style.backgroundColor = `orangered`;
+    if (i % 3 === 0) row.style.backgroundColor = `blue`;
+  });
+});
+
+// 287,460,000,000
+const diameterSolarSystem = 287_460_000_000;
+console.log(diameterSolarSystem);
+
+const priceInCents = 345_99;
+console.log(priceInCents);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.1415;
+console.log(PI);
+
+console.log(Number(`230000`));
+console.log(Number(`230_000`));
+console.log(parseInt(`230_000`));
+
+console.log(2 ** 53 - 1);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(2 ** 53 + 1);
+console.log(2 ** 53 + 2);
+console.log(2 ** 53 + 3);
+console.log(2 ** 53 + 4);
+
+console.log(4567654567865456787656787656n);
+console.log(BigInt(4567654));
+
+// Operations
+console.log(10000n + 10000n);
+console.log(1000045678765456786545n * 1000034567876543456765434567654n);
+// console.log(Math.sqrt(16n));
+
+const huge = 345676543456765456787675n;
+const number = 23;
+console.log(huge * BigInt(number));
+console.log(20n > 15);
+
+// Exceptions
+console.log(20n === 20);
+console.log(typeof 20n);
+console.log(20n == 20);
+
+console.log(huge + ` is REALLY big!!!`); // n зникає в кінці рядка
+console.log(20 + ` is REALLY big!!!`);
+
+// Divisions
+console.log(10n / 3n);
+console.log(10 / 3);
+*/
+
+// Create a date
+/*
+const now = new Date();
+console.log(now);
+
+console.log(new Date(`Wed Mar 25 2026 20:06:43 GMT+0100`));
+console.log(new Date(`December 24, 2015`));
+
+console.log(new Date(account1.movementsDates[0]));
+
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 33, 15, 23, 5));
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+// Working with dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142253380000));
+
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future);
+*/
