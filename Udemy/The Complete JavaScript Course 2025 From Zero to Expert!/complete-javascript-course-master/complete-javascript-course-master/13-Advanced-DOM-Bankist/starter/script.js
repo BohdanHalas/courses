3caeabdd41242cdc5e391c337eece7cd0e18e45e
Bohdan Halas mac <bohdan.halas10@gmail.com>
2026-04-23 19:55:27 +0200

    after lectures about scrolling and attributes/styles/Classes

--- Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -70,3 +70,82 @@ document
     // message.remove();
     message.parentElement.removeChild(message);
   });
+
+// Styles
+message.style.backgroundColor = `#37383d`;
+message.style.width = '120%';
+
+console.log(message.style.height);
+console.log(message.style.backgroundColor);
+console.log(message.style.color);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + `px`;
+
+document.documentElement.style.setProperty(`--color-primary`, `orangered`);
+
+// Attributes
+const logo = document.querySelector(`.nav__logo`);
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo?.className);
+
+logo.alt = `Beautiful logo`;
+
+// Non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute(`designer`));
+logo.setAttribute(`company`, `Bankist`);
+
+console.log(logo.src);
+console.log(logo?.getAttribute(`src`));
+
+const link = document.querySelector(`.nav__link--btn`);
+console.log(link.href);
+console.log(link?.getAttribute(`href`));
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add(`c`, `j`);
+logo.classList.remove(`c`, `j`);
+logo.classList.toggle(`c`);
+logo.classList.contains(`c`);
+
+// DON'T USE
+logo.className = `jonas`;
+
+const buttonForScrolling = document.querySelector(`.btn--scroll-to`);
+const section1 = document.querySelector('#section--1');
+
+buttonForScrolling?.addEventListener('click', function (e) {
+  const section1Coords = section1?.getBoundingClientRect();
+  console.log(section1Coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    `Current height/width viewport`,
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth,
+  );
+
+  // Scrolling
+  // window.scrollTo(
+  //   section1Coords?.left + window.pageXOffset,
+  //   section1Coords?.top + window.pageYOffset,
+  // );
+  // window.scrollTo({
+  //   left: section1Coords.left + window.pageXOffset,
+  //   top: section1Coords?.top + window.pageYOffset,
+  //   behavior: `smooth`,
+  // });
+
+  section1?.scrollIntoView({ behavior: 'smooth' });
+});
