2c75d9c88ef6acc8d2391f2e2a60ee3b47c7299a
Bohdan Halas mac <bohdan.halas10@gmail.com>
2026-04-24 20:41:34 +0200

    after lecture about matimulations with evenListener

--- Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -149,3 +149,18 @@ buttonForScrolling?.addEventListener('click', function (e) {
 
   section1?.scrollIntoView({ behavior: 'smooth' });
 });
+const h1 = document.querySelector(`h1`);
+
+const alertH1 = function (e) {
+  alert(`addEventListener: Nice! You are reading the heading!`);
+  // h1?.removeEventListener(`mouseenter`, alertH1);
+};
+h1?.addEventListener(`mouseenter`, alertH1);
+
+// h1.onmouseenter = function (e) {
+//   alert('onmouseenter: Nice! You are reading the heading!');
+// };
+
+setTimeout(function () {
+  h1?.removeEventListener(`mouseenter`, alertH1);
+}, 5 * 1000);
