eb29203e711c120a42940d168dc62ddb223fbb66
Bohdan.Halas <bohdan.halas10@gmail.com>
2026-05-28 19:42:41 +0200

    after lecture about Constructor Functions

--- Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/14-OOP/starter/script.js
+++ Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  //   console.log(this);
+
+  //Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never to do this!!!
+  //   this.calcAge = function () {
+  //     console.log(2026 - this.birthYear);
+  //   };
+};
+
+const bohdan = new Person('Bohdan', 1999);
+console.log(bohdan);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const nelya = new Person(`Nelya`, 1999);
+const jack = new Person(`Jack`, 1975);
+
+console.log(nelya, jack);
+
+const jay = 'Jay';
+
+console.log(bohdan instanceof Person);
+console.log(jay instanceof Person);
