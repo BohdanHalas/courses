28d9d57e2d4eedfca68026bcaaa6d04f3b31b1c7
Bohdan Halas mac <bohdan.halas10@gmail.com>
2026-06-16 20:45:32 +0200

    after lectures about refactroing + creating classes

--- Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/15-Mapty/starter/script.js
+++ Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/15-Mapty/starter/script.js
@@ -11,68 +11,128 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map;
-let mapEvent;
-// getting current location
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
 
-if (navigator.geolocation)
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      console.log(position);
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+  calcPace() {
+    // min/kn
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+// const run1 = new Running([40, 30], 5.2, 24, 178);
+// const cycling1 = new Cycling([40, 30], 27, 95, 523);
 
-      const coords = [latitude, longitude];
-      map = L.map('map').setView([...coords], 13);
-      console.log(map);
-      L.tileLayer(
-        'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-        {
-          attribution:
-            'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EAP, and the GIS User Community',
+// console.log(run1, cycling1);
+//////////////////////////////////
+//////////////////////////////////
+
+class App {
+  #map;
+  #mapEvent;
+  constructor() {
+    this._getPosition();
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
+
+  _getPosition() {
+    // getting current location
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          //   alert(`Could not get your position`);
         },
-      ).addTo(map);
-      // Handling click on maps event
-      map.on('click', function (mapE) {
-        mapEvent = mapE;
-        form.classList.remove('hidden');
-        inputDistance.focus();
-      });
-    },
-    function () {
-      //   alert(`Could not get your position`);
-    },
-  );
-form?.addEventListener('submit', function (e) {
-  // clear input fields
-  inputCadence.value =
-    inputDuration.value =
-    inputDistance.value =
-    inputElevation.value =
-      '';
-  //display market
-  e.preventDefault();
-  console.log(mapEvent);
-  const { lat: latitude, lng: longitude } = mapEvent.latlng;
-  console.log(latitude, longitude);
-  L.marker([latitude, longitude])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 300,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: `running-popup`,
-      }),
-    )
-    .setPopupContent('Running')
-    .openPopup();
-});
+      );
+  }
 
-inputType.addEventListener('change', function (e) {
-  e.preventDefault();
-  inputElevation.closest('.form__row')?.classList.toggle('form__row--hidden');
-  inputCadence.closest('.form__row')?.classList.toggle('form__row--hidden');
-});
+  _loadMap(position) {
+    console.log(position);
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+    const coords = [latitude, longitude];
+    console.log(this);
+    this.#map = L.map('map').setView([...coords], 13);
+    console.log(this.#map);
+    L.tileLayer(
+      'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+      {
+        attribution:
+          'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EAP, and the GIS User Community',
+      },
+    ).addTo(this.#map);
+    // Handling click on maps event
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _toggleElevationField(e) {
+    e.preventDefault();
+    inputElevation.closest('.form__row')?.classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row')?.classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e) {
+    e.preventDefault();
+    // console.log(this);
+    // clear input fields
+    inputCadence.value =
+      inputDuration.value =
+      inputDistance.value =
+      inputElevation.value =
+        '';
+    //display market
+    e.preventDefault();
+    console.log(this.#mapEvent);
+    const { lat: latitude, lng: longitude } = this.#mapEvent.latlng;
+    console.log(latitude, longitude);
+    L.marker([latitude, longitude])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 300,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: `running-popup`,
+        }),
+      )
+      .setPopupContent('Running')
+      .openPopup();
+  }
+}
+
+const app = new App();
