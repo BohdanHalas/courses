5cf20073d0bf403820aef3188a45d98a3be841ac
Bohdan Halas mac <bohdan.halas10@gmail.com>
2026-08-12 19:23:32 +0200

    after lectures about Module Pattertn and CommonJS Modules and a few commands in terminal

--- Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,6 +1,5 @@
 'use strict';
 /*
- */
 // Importing module
 
 // import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
@@ -43,3 +42,31 @@ const getLastPost = async function () {
 
 const lastPost = await getLastPost();
 console.log(lastPost);
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 267;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push(product, quantity);
+    console.log(`${quantity} ${product} added to the cart`);
+  };
+
+  const orderStock = function (product, quantity) {
+    cart.push(product, quantity);
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+ */
