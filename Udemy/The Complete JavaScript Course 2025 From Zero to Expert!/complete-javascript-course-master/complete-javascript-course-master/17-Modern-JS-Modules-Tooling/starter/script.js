142daeafc95c6cc8770ad45c225223b099ee14a4
Bohdan.Halas <bohdan.halas10@gmail.com>
2026-08-19 20:57:24 +0200

    after lecture about intro to NPM

--- Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -70,3 +70,22 @@ const ShoppingCart2 = (function () {
 ShoppingCart2.addToCart('apple', 4);
 ShoppingCart2.addToCart('pizza', 2);
  */
+
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: `bread`, quantity: 5 },
+    { product: `pizza`, quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+// state.user.loggedIn = false;
+console.log(stateClone);
+
+const stateDeepClone = cloneDeep(state);
+state.user.loggedIn = false;
+console.log(state);
+console.log(stateDeepClone);
