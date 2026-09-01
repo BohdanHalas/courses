96e373ab6028edb47569ea15c57685faf9c5cb4d
Bohdan Halas <bohdan.halas@MacBook-Pro.local>
2026-09-01 20:44:14 +0200

    after loading a recipe from first API

--- Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/18-forkify/starter/src/js/controller.js
+++ Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/18-forkify/starter/src/js/controller.js
@@ -12,3 +12,32 @@ const timeout = function (s) {
 // https://forkify-api.jonas.io
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const response = await fetch(
+      'https://forkify-api.jonas.io/api/v2/recipes/664c8f193e7aa067e94e8906',
+      // 'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886',
+    );
+    const data = await response.json();
+    if (!response.ok) throw new Error(`${data.message} (${response.status})`);
+    console.log(response, data);
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.coking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
+// fetch('https://forkify-api.jonas.io/api/v2/recipes/:id');
