b098b92ad363dfbd008b3704d1bfd6d96e7cbae3
Bohdan.Halas <bohdan.halas10@gmail.com>
2026-09-11 18:58:41 +0200

    Fix: Remove node_modules from git tracking

--- Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ Udemy/The Complete JavaScript Course 2025 From Zero to Expert!/complete-javascript-course-master/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -95,3 +95,18 @@ console.log(stateDeepClone);
 if (module.hot) {
   module.hot.accept();
 }
+
+class Person {
+  #greeting = `Hey`;
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.#greeting}, ${this.name}!`);
+  }
+}
+const bohdan = new Person(`Bohdan`);
+
+console.log(`Bohdan` ?? null);
+
+console.log(cart.find(el => el.quantity >= 2));
+
+Promise.resolve(`TEST`).then(x => console.log(x));
